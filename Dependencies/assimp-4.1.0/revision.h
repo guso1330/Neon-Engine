474c9e85f2700d25ad1b54da02eaba41029c1299
Augustus Oberdick <Augustus_Oberdick@anfcorp.com>
2019-07-23 15:30:54 -0400

    ne-mac-ecs: updating git ignore

--- Dependencies/assimp-4.1.0/revision.h
+++
@@ -1,7 +0,0 @@
-#ifndef ASSIMP_REVISION_H_INC
-#define ASSIMP_REVISION_H_INC
-
-#define GitVersion 0xb066ec2
-#define GitBranch "ne-mac-ecs"
-
-#endif // ASSIMP_REVISION_H_INC
--- Dependencies/assimp-4.1.0/test/headercheck/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aroberd/Documents/Neon-Engine/Dependencies/assimp-4.1.0/test/headercheck
-# Build directory: /Users/aroberd/Documents/Neon-Engine/Dependencies/assimp-4.1.0/test/headercheck
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

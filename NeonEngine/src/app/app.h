3a928cfde68ff3044d216bfdac085f656ceeaad3
Augustus Oberdick <Augustus_Oberdick@anfcorp.com>
2019-07-24 09:57:34 -0400

    ne-mac-ecs: removed some unused code that isn't being used yet

--- NeonEngine/src/app/app.h
+++ NeonEngine/src/app/app.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "./scene.h"
 #include "./window.h"
 
 #include <vector>
@@ -11,13 +10,6 @@ namespace neon {
 			Application();
 			~Application();
 
-			inline const std::vector<Scene*> &GetScene() const { return m_scenes; };
-
-			void Add(Scene* n_scene);
-			
-			// void Load(Scene* n_scene);
-
 		private:
-			std::vector<Scene*> m_scenes;
 	};
 }
